30782de05da80683db9475c55cced737a8546a9b
jackhannan-hello <jack.hannan@mymail.champlain.edu>
2026-09-03 16:12:08 -0400

    more updating for sign in

--- src/main/java/org/half/Main.java
+++ src/main/java/org/half/Main.java
@@ -2,26 +2,10 @@ package org.half;
 
 import org.half.utility.BankScanner;
 import org.half.view.MainMenu;
+import org.half.view.SignIn;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Bank 50!");
-        System.out.println("Are you a member of our Bank? Yes or No");
-
-        String input = BankScanner.getString().toLowerCase();
-
-        if (input.charAt(0) == 'n' ){
-            //Call the Registration Page
-        }
-
-        System.out.println("Please enter your Account ID.");
-        int userAccountID = BankScanner.getInt();
-        //Check to make sure that is a valid Account ID
-        System.out.println("Please enter your PIN number.");
-        int userPinNum = BankScanner.getInt();
-        //Check to make sure that PIN number is correct of the Account ID
-
-        MainMenu.mainMenu();
-        BankScanner.closeScanner();
+        SignIn.signIn();
     }
 }
