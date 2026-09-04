cdf7ea2a30e9ef6bdedb691392d75f0519e98d09
Darsh-KP <pateldarsh919@gmail.com>
2026-09-04 16:47:42 -0400

    Created few baked in users for testing.

--- src/main/java/org/half/repository/UserRepository.java
+++ src/main/java/org/half/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package org.half.repository;
 
 import org.half.model.User;
+import org.half.security.PasswordService;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,6 +10,17 @@ public class UserRepository {
     private static final Map<String, User> userDatabase = new HashMap<>();
 
     private UserRepository() {}
+
+    public static void initializeDatabase() {
+        for (int i = 0; i < 5; i++) {
+            User newUser = new User();
+            newUser.setUsername("test" + i);
+            newUser.setPassword(PasswordService.hashPassword("testPassword" + i));
+            newUser.setBalance(500.00 * i);
+
+            addUser(newUser);
+        }
+    }
 
     public static void addUser(User user) {
         userDatabase.put(user.username, user);
