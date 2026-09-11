63cf587a5e42f94b2c4532d1133f90e720f03b89
jackhannan-hello <jack.hannan@mymail.champlain.edu>
2026-09-11 18:08:39 -0400

    updating signin to work with the database

--- src/main/java/org/half/repository/UserRepository.java
+++ src/main/java/org/half/repository/UserRepository.java
@@ -5,11 +5,12 @@ import org.half.utility.ConnectionFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserRepository {
     public static void addUser(User user) {
-        String query = "INSERT INTO Users (firstName, lastName, email, phoneNumber, username, password) VALUES (?, ?, ?, ?, ?, ?)";
+        String query = "INSERT INTO User (fName, lName, email, phone, username, passwordHash) VALUES (?, ?, ?, ?, ?, ?)";
         try (
             Connection connection = ConnectionFactory.getAutoCommitConnection();
             PreparedStatement statement = connection.prepareStatement(query);
@@ -24,5 +25,50 @@ public class UserRepository {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public static String getPasswordHash(String userName){
+        String query = "SELECT passwordHash FROM User WHERE username=?";
+        try (
+                Connection connection = ConnectionFactory.getAutoCommitConnection();
+                PreparedStatement statement = connection.prepareStatement(query);
+
+        ) {
+            statement.setString(1, userName);
+            ResultSet resultSet = statement.executeQuery();
+            if(resultSet.next()){
+                return resultSet.getString("passwordHash");
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static User getUser(String userName) {
+        String query = "SELECT * FROM User WHERE username=?";
+        try (
+                Connection connection = ConnectionFactory.getAutoCommitConnection();
+                PreparedStatement statement = connection.prepareStatement(query);
+
+        ) {
+            statement.setString(1, userName);
+            ResultSet resultSet = statement.executeQuery();
+            if(resultSet.next()){
+                User user = new User(
+                        resultSet.getString("fName"),
+                        resultSet.getString("lName"),
+                        resultSet.getString("email"),
+                        resultSet.getString("phone"),
+                        resultSet.getString("username"),
+                        resultSet.getString("passwordHash")
+                );
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
