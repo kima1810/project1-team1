72e4fe302163c4631070feec04dbb7c7f2f335c7
Alex <alexykim1101@gmail.com>
2026-09-17 19:05:48 -0400

    Removed RegisterTest, Created UserServiceTest, Implemented checks in UserService

--- src/main/java/org/half/service/UserService.java
+++ src/main/java/org/half/service/UserService.java
@@ -6,6 +6,14 @@ import org.half.repository.UserRepository;
 
 public class UserService {
     public static User createUser(String firstName, String lastName, String email, String phoneNumber, String username, String password) {
+        if (firstName == null || firstName.isBlank() || firstName.length() > 20
+            || lastName == null || lastName.isBlank() || lastName.length() > 20
+            || email == null || !email.matches(".*@.*\\..*") || UserRepository.getUserByEmail(email) != null
+            || phoneNumber == null || !phoneNumber.matches("[0-9+()\\- ]+")
+            || username == null || username.length() < 5 || username.length() > 50
+            || password == null || password.length() > 255) {
+            return null;
+        }
         try{
             User user = new User(firstName, lastName, email, phoneNumber, username, password);
             UserRepository.addUser(user);
