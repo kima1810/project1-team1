8480af5963495ceffe6b6b0d37cc7eeed51da278
jackhannan-hello <jack.hannan@mymail.champlain.edu>
2026-09-17 16:03:19 -0400

    implementing the logging for SignIn and also starting and closing the application

--- src/main/java/org/half/Main.java
+++ src/main/java/org/half/Main.java
@@ -1,13 +1,18 @@
 package org.half;
 
-import org.half.repository.UserRepository;
 import org.half.utility.BankScanner;
+import org.half.utility.PromptUserSelection;
 import org.half.view.SignIn;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        //UserRepository.initializeDatabase();
+        log.info("Bank application started");
         SignIn.signIn();
         BankScanner.closeScanner();
+        log.info("Bank application shutting down");
     }
 }
