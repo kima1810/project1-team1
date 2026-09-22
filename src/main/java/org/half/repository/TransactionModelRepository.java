3d6386214b94a20d7be9a25659933801c53de46e
Gavin Garcia <gavinedwingarcia@gmail.com>
2026-09-22 15:26:58 -0500

    test lol

--- src/main/java/org/half/repository/TransactionModelRepository.java
+++ src/main/java/org/half/repository/TransactionModelRepository.java
@@ -54,6 +54,63 @@ public class TransactionModelRepository {
         logger.info("History successfully retrieved, no issues with the database");
         return transactionList;
     }
+
+
+    //attempt at creating the method to display the transaction history data in different ways
+    //if items are found, it will return a list of the transactions
+    /*
+    public List<TransactionModel> printOutTransactionsCustom(long id, String typeDisplayed, boolean dateOrAmount, boolean ASCorDESC){
+        //create the query
+        String query;
+
+        if(typeDisplayed.equals("Deposit")){
+            if()
+        }
+
+        else if(typeDisplayed.equals("Withdrawal")){
+
+        }
+
+        else if(typeDisplayed.equals("Transfer")){
+
+        }
+
+        else{
+            query = "SELECT dateTime, type, amount, originAccountNumber, destinationAccountNumber " +
+                    "FROM TransactionHistory WHERE originAccountNumber=? OR destinationAccountNumber=?" + "ORDER BY dateTime DESC;";
+        }
+        //create the list container
+        List<TransactionModel> transactionList = new ArrayList<>();
+        //check the connection
+        try (Connection connection = ConnectionFactory.getAutoCommitConnection();
+             PreparedStatement statement = connection.prepareStatement(query)
+
+        ) {
+            //check if a value's originAccountNumber or destinationAccountNumber match the imputed it
+            statement.setLong(1, id);
+            statement.setLong(2, id);
+            ResultSet resultSet = statement.executeQuery();
+            //add it to the list
+            while(resultSet.next()) {
+                transactionList.add(new TransactionModel(
+                        resultSet.getString("dateTime"),
+                        resultSet.getString("type"),
+                        resultSet.getDouble("amount"),
+                        resultSet.getLong("originAccountNumber"),
+                        resultSet.getLong("destinationAccountNumber")
+                ));
+            }
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+            logger.error("Error with interacting with the database to check the transaction history");
+
+        }
+        logger.info("History successfully retrieved, no issues with the database");
+        return transactionList;
+    }
+*/
+
     //inserts a deposit or withdrawal into the table
     public boolean addDepositOrWithdrawal(TransactionModel transactionModel){
         //the query
