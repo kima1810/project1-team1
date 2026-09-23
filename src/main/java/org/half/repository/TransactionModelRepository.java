a936a876a0598f05001e0a26136c6de3201948e9
Gavin Garcia <gavinedwingarcia@gmail.com>
2026-09-23 17:23:25 -0500

    Made it so that the transaction history only prints out 10 transactions at a time and so the user can scroll between pages using the space bar and backspace. I also fixed the test cases for the transactionHistory so that they actually run.

--- src/main/java/org/half/repository/TransactionModelRepository.java
+++ src/main/java/org/half/repository/TransactionModelRepository.java
@@ -16,10 +16,11 @@ public class TransactionModelRepository {
     private static final Logger logger = LoggerFactory.getLogger(TransactionModelRepository.class);
 
     //if items are found, it will return a list of the transactions
+    /*
     public List<TransactionModel> printOutTransactions(long id){
         //create the query
         String query = "SELECT dateTime, type, amount, originAccountNumber, destinationAccountNumber " +
-                "FROM TransactionHistory WHERE originAccountNumber=? OR destinationAccountNumber=?" + "ORDER BY dateTime DESC;";
+                "FROM TransactionHistory WHERE originAccountNumber=? OR destinationAccountNumber=? " + "ORDER BY dateTime DESC;";
 
         //create the list container
         List<TransactionModel> transactionList = new ArrayList<>();
@@ -55,27 +56,95 @@ public class TransactionModelRepository {
 
     //attempt at creating the method to display the transaction history data in different ways
     //if items are found, it will return a list of the transactions
-    /*
-    public List<TransactionModel> printOutTransactionsCustom(long id, String typeDisplayed, boolean dateOrAmount, boolean ASCorDESC){
+
+    public List<TransactionModel> printOutTransactionsCustom(
+            long id,
+            String typeDisplayed,
+            boolean orderByDateAndNotAmount,
+            boolean useDESCAndNotASC,
+            int limit,
+            int offset) {
+
+        String query =
+                "SELECT dateTime, type, amount, originAccountNumber, destinationAccountNumber " +
+                        "FROM TransactionHistory " +
+                        "WHERE (originAccountNumber=? OR destinationAccountNumber=?) ";
+
+        // Only filter by transaction type if a valid type was provided
+        boolean filterByType =
+                typeDisplayed.equals("Deposit") ||
+                        typeDisplayed.equals("Withdrawal") ||
+                        typeDisplayed.equals("Transfer");
+
+        if (filterByType) {
+            query += "AND type=? ";
+        }
+
+        // Choose what to sort by
+        if (orderByDateAndNotAmount) {
+            query += "ORDER BY dateTime ";
+        } else {
+            query += "ORDER BY amount ";
+        }
+
+        // Choose ascending or descending
+        if (useDESCAndNotASC) {
+            query += "DESC ";
+        } else {
+            query += "ASC ";
+        }
+
+        // Pagination
+        query += "LIMIT ? OFFSET ?";
+
+        List<TransactionModel> transactionList = new ArrayList<>();
+
+        try (Connection connection = ConnectionFactory.getAutoCommitConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            int parameterIndex = 1;
+
+            // Account number
+            statement.setLong(parameterIndex++, id);
+            statement.setLong(parameterIndex++, id);
+
+            // Transaction type, if applicable
+            if (filterByType) {
+                statement.setString(parameterIndex++, typeDisplayed);
+            }
+
+            // Pagination
+            statement.setInt(parameterIndex++, limit);
+            statement.setInt(parameterIndex, offset);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                transactionList.add(new TransactionModel(
+                        resultSet.getString("dateTime"),
+                        resultSet.getString("type"),
+                        resultSet.getDouble("amount"),
+                        resultSet.getLong("originAccountNumber"),
+                        resultSet.getLong("destinationAccountNumber")
+                ));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            logger.error("Error with interacting with the database to retrieve transaction history");
+        }
+
+        logger.info("History successfully retrieved, no issues with the database");
+
+        return transactionList;
+    }
+    */
+
+    public List<TransactionModel> printOutTransactions(long id, int limit, int offset){
         //create the query
-        String query;
+        String query = "SELECT dateTime, type, amount, originAccountNumber, destinationAccountNumber " +
+                "FROM TransactionHistory WHERE originAccountNumber=? OR destinationAccountNumber=? " + "ORDER BY dateTime DESC " + "LIMIT ? OFFSET ?;";
 
-        if(typeDisplayed.equals("Deposit")){
-            if()
-        }
-
-        else if(typeDisplayed.equals("Withdrawal")){
-
-        }
-
-        else if(typeDisplayed.equals("Transfer")){
-
-        }
-
-        else{
-            query = "SELECT dateTime, type, amount, originAccountNumber, destinationAccountNumber " +
-                    "FROM TransactionHistory WHERE originAccountNumber=? OR destinationAccountNumber=?" + "ORDER BY dateTime DESC;";
-        }
         //create the list container
         List<TransactionModel> transactionList = new ArrayList<>();
         //check the connection
@@ -86,6 +155,8 @@ public class TransactionModelRepository {
             //check if a value's originAccountNumber or destinationAccountNumber match the imputed it
             statement.setLong(1, id);
             statement.setLong(2, id);
+            statement.setInt(3, limit);
+            statement.setInt(4, offset);
             ResultSet resultSet = statement.executeQuery();
             //add it to the list
             while(resultSet.next()) {
@@ -106,7 +177,6 @@ public class TransactionModelRepository {
         logger.info("History successfully retrieved, no issues with the database");
         return transactionList;
     }
-*/
 
     //inserts a deposit or withdrawal into the table
     public boolean addDepositOrWithdrawal(TransactionModel transactionModel){
